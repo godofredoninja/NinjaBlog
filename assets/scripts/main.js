91df83d0b7364f4e1b696b82725279eb986d79c6
GodoFredo <godofredo720@hotmail.com>
2015-02-15 16:41:33 -0500

    add prismjs para colorear codigos

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -19,4 +19,12 @@
     result_template : "<div class='searchResults'><a href='{{link}}'><p><h4>{{title}}</h4></p></a></div>",
     onKeyUp         : true
   });
+
+  // Facebook Embed
+  $('.fb-post').wrap( '<div class="Facebook-embed"></div>');
+
+  // Facebook Embed
+  $('.twitter-tweet').wrap( '<div class="Twitter-embed"></div>');
+
+
 })();
